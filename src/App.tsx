e9b0172e0f2c125adcd19b0f45d22d8b2e100245
LJMunt <muntwyler.lukas@musys.io>
2026-01-24 12:05:29 +0100

    Initial Vite + React + TypeScript Setup

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+    return (
+        <div>
+            <h1>GoTodo Web</h1>
+            <p>If you can see this, React works.</p>
+        </div>
+    );
+}
+
+export default App;
